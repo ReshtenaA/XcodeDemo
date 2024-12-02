a4a2ef4593911c3a8dc996a361c7fac4af623762
ReshtenaA <rarghandiwal@sfsu.edu>
2024-12-02 10:22:11 -0800

    Initial Commit

---
+++ XcodeDemo/XcodeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoApp.swift
+//  XcodeDemo
+//
+//  Created by Reshtena Arghandiwal on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
